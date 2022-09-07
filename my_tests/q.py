864a952981a8f2cd803261707c921c1d2c7b401a
JeoaFesketto <fesquet.jean18@gmail.com>
2022-09-07 17:08:37 +0200

    relative paths

--- my_tests/q.py
+++ my_tests/q.py
@@ -1,5 +1,5 @@
 import parablade as pb
 
-details_dict = pb.ReadUserInput(r'/home/daep/j.fesquet/git_repos/parablade/testcases/MatchBlades/NASA_R67/NASA_R67.cfg')
+details_dict = pb.ReadUserInput(r'./testcases/MatchBlades/NASA_R67/NASA_R67.cfg')
 o = pb.BladeMatch(details_dict)
 o.match_blade(matching_mode='DVs')
