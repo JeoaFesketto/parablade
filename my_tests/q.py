f994b27fe20086cf11ce520403008cb62f6d052b
JeoaFesketto <fesquet.jean18@gmail.com>
2022-09-07 17:32:01 +0200

    paths

--- my_tests/q.py
+++ my_tests/q.py
@@ -1,5 +1,5 @@
 import parablade as pb
 
-details_dict = pb.ReadUserInput(r'./testcases/MatchBlades/Aachen_2D/Aachen_2D.cfg')
+details_dict = pb.ReadUserInput(r'./testcases/MatchBlades/Aachen_2D/Aachen_3D.cfg')
 o = pb.BladeMatch(details_dict)
 o.match_blade(matching_mode='DVs')
