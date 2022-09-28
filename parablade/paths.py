e0775384514d04c1bd285cf11c92601d1f33677f
JeoaFesketto <fesquet_jean@yahoo.fr>
2022-09-28 14:32:29 +0200

    cleaned up stuff

---
+++ parablade/paths.py
@@ -0,0 +1,12 @@
+# paths to useful test_cases
+
+AACHEN_2D = [
+    '/home/daep/j.fesquet/git_repos/parablade/testcases/MatchBlades/Aachen_2D/Aachen_2D.cfg',
+    '/home/daep/j.fesquet/git_repos/parablade/testcases/MatchBlades/Aachen_2D/MoveSurface.txt'
+]
+
+AACHEN_3D = [
+    '/home/daep/j.fesquet/git_repos/parablade/testcases/MatchBlades/Aachen_3D/Aachen_3D.cfg',
+    '/home/daep/j.fesquet/git_repos/parablade/testcases/MatchBlades/Aachen_3D/MoveSurface.txt'
+]
+
